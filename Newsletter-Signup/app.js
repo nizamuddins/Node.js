9217e6fc0cbebfc23f200cc0ac1b2a247936ca54
nizamuddins <uddinmdnizam61@gmail.com>
2022-07-16 02:14:27 +0530

    project

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -3,10 +3,22 @@ const bodyParsrer = require('body-parser');
 const request = require('request');
 
 const app = express();
+app.use(bodyParsrer.urlencoded({extended:true}))
+
+
 app.use(express.static('public'))
 app.get('/',(req,res)=>{
 
     res.sendFile(__dirname+'/signup.html')
+})
+
+app.post('/',(req,res)=>{
+const  firstName   = req.body.firstname
+const  lastNmae   = req.body.lastname
+const  emailAddress = req.body.emailaddress
+console.log(firstName,lastNmae,emailAddress)
+
+
 })
 
 
