f861298af238fd67aff864356cefc43e91b1de01
nizamuddins <uddinmdnizam61@gmail.com>
2022-07-18 02:03:26 +0530

    project

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -2,7 +2,10 @@ const express = require('express');
 const bodyParsrer = require('body-parser');
 const request = require('request');
 
+const https = require('https');
+
 const app = express();
+
 app.use(bodyParsrer.urlencoded({extended:true}))
 
 
@@ -16,12 +19,57 @@ app.post('/',(req,res)=>{
 const  firstName   = req.body.firstname
 const  lastNmae   = req.body.lastname
 const  emailAddress = req.body.emailaddress
-console.log(firstName,lastNmae,emailAddress)
+
+const data = {
+
+members:[
+{
+    email_address:emailAddress,
+    status:"subscribed",
+    merge_fields:{
+    FNAME:firstName,
+    LNAME:lastNmae,
+  }  
+}
+
+]
 
 
+}
+
+const jsondata = JSON.stringify(data);
+const url = "https://us11.api.mailchimp.com/3.0/lists/454e9390d8/";
+
+const option={
+method:'POST',
+auth:'nizam:ca1d4868b500b72211f532a38f032122-us11',
+}
+
+
+const request = https.request(url,option,(response)=>{
+if(response.statusCode === 200){
+    res.send('succesfull')
+}else{
+    res.send('unsuccessfull')
+}
+
+response.on('data',(data)=>{
+    console.log(JSON.parse(data))
+})
+
+})
+request.write(jsondata);
+request.end();
 })
 
 
 app.listen(3000,()=>{
     console.log('server is activated')
 })
+
+
+// API key
+// d58aa9def308015717e4e39ae74a241b-us11
+
+// audience_id
+// 454e9390d8
