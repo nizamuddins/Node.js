fc03c1b90e1acde871c9b9d61aacd62afde2ca10
nizamuddins <uddinmdnizam61@gmail.com>
2022-07-20 01:13:10 +0530

    project

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -3,6 +3,7 @@ const bodyParsrer = require('body-parser');
 const request = require('request');
 
 const https = require('https');
+const { dirname } = require('path');
 
 const app = express();
 
@@ -48,13 +49,15 @@ auth:'nizam:ca1d4868b500b72211f532a38f032122-us11',
 
 const request = https.request(url,option,(response)=>{
 if(response.statusCode === 200){
-    res.send('succesfull')
+    res.sendFile(__dirname+'/success.html')
 }else{
-    res.send('unsuccessfull')
+    res.sendFile(__dirname+'/failure.html')
+
 }
 
 response.on('data',(data)=>{
     console.log(JSON.parse(data))
+
 })
 
 })
@@ -62,14 +65,16 @@ request.write(jsondata);
 request.end();
 })
 
+app.post('/failure',(req,res)=>{
+res.redirect('/');
+})
 
 app.listen(3000,()=>{
     console.log('server is activated')
 })
 
 
-// API key
-// d58aa9def308015717e4e39ae74a241b-us11
+
 
 // audience_id
 // 454e9390d8
