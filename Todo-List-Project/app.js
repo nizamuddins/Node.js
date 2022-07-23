cad8bd827e3039096a7a7367ced58fa02bca617b
nizamuddins <uddinmdnizam61@gmail.com>
2022-07-24 02:28:46 +0530

    project

--- Todo-List-Project/app.js
+++ Todo-List-Project/app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 var items =[];
+var newitems = [];
 app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine','ejs');
 app.use(express.static('public'));
@@ -18,10 +19,24 @@ res.render('list',{days:day,newItem:items});
  
 })
 app.post('/',(req,res)=>{
-let item = req.body.text;
-items.push(item);
-res.redirect('/')
+    if(req.body.button==='work'){
+        let items2 = req.body.text;
+        newitems.push(items2);
+
+        res.redirect('/work');
+    }else{
+        let item = req.body.text;
+        items.push(item);
+        res.redirect('/');
+    }
+
 })
+
+app.get('/work',(req,res)=>{
+
+res.render('list',{days:'work list',newItem:newitems})
+})
+
 
 app.listen(3000,()=>{
     console.log("server is started");
