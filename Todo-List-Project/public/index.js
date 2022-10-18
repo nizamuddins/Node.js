5ac75c7e6cd9b8568bd156090ec293d57507b9e8
nizamuddins <uddinmdnizam61@gmail.com>
2022-10-19 01:52:34 +0530

    nodejs

--- Todo-List-Project/public/index.js
+++ Todo-List-Project/public/index.js
@@ -1,3 +1,4 @@
+let body = $("body")
 $(document).keydown((e)=>{
     if (e.ctrlKey==true && (e.which == '61' || e.which == '107' || e.which == '173' || e.which == '109'  || e.which == '187'  || e.which == '189'  ) ) {
             e.preventDefault();
@@ -14,3 +15,27 @@ $(document).keydown((e)=>{
            }
 });
 
+let width = window.innerWidth;
+
+if(width < 1230){
+
+
+$(".box").remove();
+body.css("background-image","none")
+body.text("Error!")
+
+
+
+}
+
+window.addEventListener("resize",(e)=>{
+if(window.innerWidth < 1426){
+    $(".box").remove();
+    body.css("background-image","none")
+
+    body.text("Error!")
+    
+    
+
+}
+})
